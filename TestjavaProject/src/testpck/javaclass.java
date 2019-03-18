3643267a260c75612cadfc5a66189b378908d2e4
benazir.begum <benazir.begum@lge.com>
2019-03-18 10:41:56 +0530

    Helloworld commit msg

---
+++ TestjavaProject/src/testpck/javaclass.java
@@ -0,0 +1,10 @@
+package testpck;
+
+public class javaclass {
+public void hwproject()
+{
+	System.out.print("~~~~~~~~~~~~~~~~~~~~~\n");
+	System.out.print("Say hello to everyone\n");
+	System.out.print("~~~~~~~~~~~~~~~~~~~~~\n");
+}
+}
